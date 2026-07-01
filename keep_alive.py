fe4f4ac2d83d393386f375760e314d9c0844b812
jaxonroblox147youtube-wq <jaxonroblox147youtube@gmail.com>
2026-07-01 08:45:25 +0000

    Add requirements file

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from threading import Thread
 
@@ -5,11 +6,13 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "I am alive!"  # This is what UptimeRobot will see
+    return "I am alive!"
 
 def run():
-    # Render uses port 10000 by default for free web services
-    app.run(host='0.0.0.0', port=10000) 
+    # CRUCIAL: Render dynamically assigns a port using an environment variable. 
+    # This reads that variable, defaulting to 10000 if it can't find it.
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
